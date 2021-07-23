ae1f6ae491dd9a6c1f35aa793f9e5c9d81ec7723
john <83730668+DevJohn2021@users.noreply.github.com>
2021-07-23 18:18:48 -0500

    ajustes material

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        <iframe width="853" height="480" src="https://www.youtube.com/embed/ccSFFRMa2Fs?list=PLkc5n6npRWkj1xpTPWZjkwOdUbd7DjHYO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       
     
 
